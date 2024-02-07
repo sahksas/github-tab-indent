cb1e4c1e93c9d41b55af09eaf19933e83241c1b7
sahksas <baku@baku.world>
2024-02-07 17:53:30 +0900

    remove unnecessary

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   chrome.action.disable(tabId);
   const url = tab.url;
-  console.log(url);
   if (url === undefined) return;
   if (url.startsWith("https://www.github.com/")) {
     chrome.action.enable(tabId);
