d1629d30294a2e97c65e2ffdbb0b7be9af53310c
sahksas <baku@baku.world>
2024-02-07 19:52:52 +0900

    fix issue #1

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,37 +1,39 @@
-const main = () => {
-  const textareaList = document.querySelectorAll("textarea");
-  for (const textarea of textareaList) {
-    textarea.addEventListener("keydown", (e) => {
-      const isSelected = textarea.selectionStart !== textarea.selectionEnd; // 選択範囲があるかどうか
-      if (!isSelected) {
-        if (e.key === "Tab" && e.shiftKey) {
-          e.preventDefault();
-          const start = textarea.selectionStart;
-          const end = textarea.selectionEnd;
-          const text = textarea.value;
-          const beforeText = text.substring(0, start);
-          const afterText = text.substring(end);
-          const lines = beforeText.split("\n");
-          const lastLine = lines[lines.length - 1];
-          const newLastLine = lastLine.replace(/^ {1,2}/, "");
-          textarea.value = beforeText.substring(0, beforeText.length - lastLine.length) + newLastLine + afterText;
-          textarea.selectionStart = textarea.selectionEnd = start - (lastLine.length - newLastLine.length);
-        } else if (e.key === "Tab") {
-          e.preventDefault();
-          const start = textarea.selectionStart;
-          const end = textarea.selectionEnd;
-          const text = textarea.value;
-          const beforeText = text.substring(0, start);
-          const afterText = text.substring(end);
-          const lines = beforeText.split("\n");
-          const lastLine = lines[lines.length - 1];
-          const newLastLine = "  " + lastLine;
-          textarea.value = beforeText.substring(0, beforeText.length - lastLine.length) + newLastLine + afterText;
-          textarea.selectionStart = textarea.selectionEnd = start + 2;
-        }
-      }
-    });
+const main = (e: KeyboardEvent): void => {
+  const activeElement = e.target as HTMLTextAreaElement;
+  const isSelected = activeElement.selectionStart !== activeElement.selectionEnd; // 選択範囲があるかどうか
+  if (!isSelected) {
+    if (e.key === "Tab" && e.shiftKey) {
+      e.preventDefault();
+      const start = activeElement.selectionStart;
+      const end = activeElement.selectionEnd;
+      const text = activeElement.value;
+      const beforeText = text.substring(0, start);
+      const afterText = text.substring(end);
+      const lines = beforeText.split("\n");
+      const lastLine = lines[lines.length - 1];
+      const newLastLine = lastLine.replace(/^ {1,2}/, "");
+      activeElement.value = beforeText.substring(0, beforeText.length - lastLine.length) + newLastLine + afterText;
+      activeElement.selectionStart = activeElement.selectionEnd = start - (lastLine.length - newLastLine.length);
+    } else if (e.key === "Tab") {
+      e.preventDefault();
+      const start = activeElement.selectionStart;
+      const end = activeElement.selectionEnd;
+      const text = activeElement.value;
+      const beforeText = text.substring(0, start);
+      const afterText = text.substring(end);
+      const lines = beforeText.split("\n");
+      const lastLine = lines[lines.length - 1];
+      const newLastLine = "  " + lastLine;
+      activeElement.value = beforeText.substring(0, beforeText.length - lastLine.length) + newLastLine + afterText;
+      activeElement.selectionStart = activeElement.selectionEnd = start + 2;
+    }
   }
 };
 
-main();
+document.addEventListener("focusin", () => {
+  const activeElement = document.activeElement;
+  if (activeElement instanceof HTMLTextAreaElement) {
+    activeElement.removeEventListener("keydown", main, true);
+    activeElement.addEventListener("keydown", main, true);
+  }
+});
